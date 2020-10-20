4ed82c92df938cd54a61c93a38b5aa0766412fca
KathyLau <kathy@mit.edu>
2020-10-20 16:42:20 -0400

    autopep 8 all

--- tests/map.py
+++ tests/map.py
@@ -1,15 +1,16 @@
-#example code from prof paine
+# example code from prof paine
 def inc(x):
     return x + 1
 
-#searchbar method that should only accept strings in search bar
+
+# searchbar method that should only accept strings in search bar
 def searchbar():
     searchvalue = "poke restaurant"
     if isinstance(searchvalue, str):
         return searchvalue
     else:
         print("Please enter a word")
-        searchbar()
+
 
 def dist():
     distvalue = 500
@@ -17,27 +18,29 @@ def dist():
         return distvalue
     else:
         print("Please enter a number")
-        dist()
+
 
 def borough():
-	bvalue = "Brooklyn"
-	if isinstance(bvalue, str):
-		return bvalue
-	else:
-		print("Please enter a word")
-		borough()
+    bvalue = "Brooklyn"
+    if isinstance(bvalue, str):
+        return bvalue
+    else:
+        print("Please enter a word")
 
-#defining a class for list objects?
+
+# defining a class for list objects?
 class Biz:
-  def __init__(self, name, location, description):
-    self.name = name
-    self.location = location
-    self.description = description
+    def __init__(self, name, location, description):
+        self.name = name
+        self.location = location
+        self.description = description
+
 
 p1 = Biz("Westside", "Morningside Heights", "Grocery store")
 p2 = Biz("Up", "Morningside Heights", "Coffee shop")
 
-#Make a multidimensional list to store shops/businesses? a nested array?
-Ls = [p1,p2]
+# Make a multidimensional list to store shops/businesses? a nested array?
+Ls = [p1, p2]
 
-#write a method to force certain fields to be filled? and filled with certain types?
+# write a method to force certain fields to be filled? and filled with
+# certain types?
--- tests/test_all.py
+++ tests/test_all.py
@@ -1,4 +1,4 @@
-#import the method from project folder
+# import the method from project folder
 from .map import inc
 from .map import searchbar
 from .map import dist
@@ -6,13 +6,16 @@ from .map import borough
 from .map import Biz
 
 
-#this is a sample test from prof paine's project
+# this is a sample test from prof paine's project
 def test_inc():
     assert inc(1) == 2, "did not pass test_inc"
 
-#below are some possible test ideas to use on ""code"" in map.py
+# below are some possible test ideas to use on ""code"" in map.py
 
-#test 1 - checks if a string is taken in from the searchbar - should always end up being a string
+# test 1 - checks if a string is taken in from the searchbar - should
+# always end up being a string
+
+
 def test_searchbar():
     searchedvalue = isinstance(searchbar(), str)
     if searchedvalue:
@@ -20,16 +23,19 @@ def test_searchbar():
     else:
         print("Fail test 1")
 
-#test 2 - check if any duplicates in list of shops
+# test 2 - check if any duplicates in list of shops
+
+
 def anydup(thelist):
-  seen = set()
-  for x in thelist:
-    if x in seen: print("Fail test 2")
-    seen.add(x)
-  return print("Pass test 2")
+    seen = set()
+    for x in thelist:
+        if x in seen:
+            print("Fail test 2")
+        seen.add(x)
+    return print("Pass test 2")
 
 
-#test 3 - check if a searched value for distance is an int
+# test 3 - check if a searched value for distance is an int
 def test_dist():
     distvalue = isinstance(dist(), int)
     if distvalue:
@@ -38,14 +44,16 @@ def test_dist():
         print("Fail test 1")
 
 
-#test 4 - check if list is empty
+# test 4 - check if list is empty
 def emptyList(thelist):
     if not thelist:
         print("Fail test 3")
     else:
         print("Pass test 3")
 
-#test 5 - check if searched value for borough is a string
+# test 5 - check if searched value for borough is a string
+
+
 def test_borough():
     b = isinstance(borough(), str)
     if b:
@@ -53,21 +61,25 @@ def test_borough():
     else:
         print("Fail test 1")
 
-#test 6 - check the actual value of the searchbar
+# test 6 - check the actual value of the searchbar
+
+
 def test2_searchbar():
     assert searchbar() == "poke restaurant"
 
-#test 7 - check the actual distance of the search
-def test_dist():
-    assert dist()== 500
+# test 7 - check the actual distance of the search
 
 
-#test 8 - check if the borough actually matches
-def test_borough():
+def test_dist2():
+    assert dist() == 500
+
+
+# test 8 - check if the borough actually matches
+def test_borough2():
     assert borough() == "Brooklyn"
-       
 
-#test 9 - test Biz class creation
+
+# test 9 - test Biz class creation
 def test_Bizclass():
     p1 = Biz("Westside", "Morningside Heights", "Grocery store")
     if isinstance(p1, Biz):
@@ -75,10 +87,13 @@ def test_Bizclass():
     else:
         print("Fail test 9")
 
-#test 10 - test Biz class comparison
+# test 10 - test Biz class comparison
+
+
 def test_Bizes():
     p1 = Biz("Westside", "Morningside Heights", "Grocery store")
     p2 = Biz("Up", "Morningside Heights", "Coffee shop")
     assert p1.location == p2.location
+
 
 test_Bizes()
