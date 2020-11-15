68ab3926fd5373a56859b35ace6c5055a62c6a05
KathyLau <kathy@mit.edu>
2020-11-15 12:11:19 -0500

    home url and register business

--- app.py
+++ app.py
@@ -1,13 +1,37 @@
 #!/usr/local/bin/python
 
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/, methods=['GET', 'POST'])
 def home():
-    return render_template('index.html', API_KEY="api-key")
+    if request.method == 'POST':
+        return render_template('index.html', API_KEY="api-key")
+    else:
+        return render_template('index.html')
+
+
+# to register a business
+@app.route('/register', methods=['GET','POST'])
+def register():
+    if request.method == 'GET':
+        return render_template('register.html')
+    elif request.method == 'POST':
+        bname = request.form['bname']
+        email = request.form['email']
+        oname = request.form['oname']
+        phone = request.form['phone']
+        address = request.form['address']
+        confirm = request.form['confirm']
+        hours = request.form['hours']
+        site = request.form['site']
+        insta = request.form['insta']
+        cuisine = request.form['cuisine']
+
+        #code to insert into database
+    return redirect(url_for('home'))
 
 
 if __name__ == '__main__':
---
+++ load.py
@@ -0,0 +1,6 @@
+import csv
+file="data/black-owned-restaurants-nyc.csv"
+with open(file, newline='') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        print (row['Address'])
