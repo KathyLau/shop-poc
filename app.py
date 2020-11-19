886e1b4d15dcb685942ef69832ecdce0b7dec46f
doria-chen <37597675+doria-chen@users.noreply.github.com>
2020-11-19 13:57:37 -0500

    removed some unused flask imports that flake8 caught

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for, request, jsonify
+from flask import Flask, render_template, request, jsonify
 app = Flask(__name__)
 
 searchterm = "Search Results"
